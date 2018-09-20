0b6e2b8a8e4663d8e22233c1face8d2a6e4d9ad0
Jamie Wright <jamie@brilliantfantastic.com>
2018-09-21 06:40:40 +1000

    Function to create or update an identity from a Slack provider

--- lib/otto/identity.ex
+++ lib/otto/identity.ex
@@ -10,6 +10,57 @@ defmodule Otto.Identity do
     timestamps()
   end
 
+  def for_access_token(provider, access_token) do
+    if identity =
+         Repo.get_by(
+           __MODULE__,
+           provider: to_string(provider),
+           access_token: access_token
+         ) do
+      {:ok, identity}
+    else
+      {:error, :not_found}
+    end
+  end
+
+  def for_uid(provider, uid) do
+    if identity =
+         Repo.get_by(
+           __MODULE__,
+           provider: to_string(provider),
+           uid: uid
+         ) do
+      {:ok, identity}
+    else
+      {:error, :not_found}
+    end
+  end
+
+  def find_or_create_from_auth(%{
+        provider: :slack,
+        credentials: %{token: access_token},
+        other: %{user: username, user_id: uid}
+      }) do
+    case(for_access_token(:slack, access_token)) do
+      {:ok, identity} ->
+        update_identity(identity, uid, username, access_token)
+
+      {:error, :not_found} ->
+        case(for_uid(:slack, uid)) do
+          {:ok, identity} ->
+            update_identity(identity, uid, username, access_token)
+
+          {:error, :not_found} ->
+            create(%{
+              provider: "slack",
+              access_token: access_token,
+              username: username,
+              uid: uid
+            })
+        end
+    end
+  end
+
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
@@ -26,5 +77,14 @@ defmodule Otto.Identity do
       :provider_access_token,
       name: :unique_provider_access_tokens
     )
+  end
+
+  defp update_identity(identity, uid, username, access_token) do
+    __MODULE__.changeset(identity, %{
+      uid: uid,
+      username: username,
+      access_token: access_token
+    })
+    |> Repo.update()
   end
 end
--- test/otto/identity_test.exs
+++ test/otto/identity_test.exs
@@ -48,4 +48,77 @@ defmodule Otto.IdentityTest do
                {"is not a supported provider", [validation: :inclusion]}
     end
   end
+
+  describe ".for_access_token" do
+    test "returns a tuple with the identity if found" do
+      Identity.create(%{provider: "slack", access_token: "ATOKEN"})
+
+      assert {:ok, identity} = Identity.for_access_token(:slack, "ATOKEN")
+      assert identity.provider == "slack"
+      assert identity.access_token == "ATOKEN"
+    end
+
+    test "returns a not found error tuple if not found" do
+      assert {:error, :not_found} = Identity.for_access_token(:slack, "blah")
+    end
+  end
+
+  describe ".for_uid" do
+    test "returns a tuple with the identity if found" do
+      Identity.create(%{provider: "slack", access_token: "ATOKEN", uid: "UID"})
+
+      assert {:ok, identity} = Identity.for_uid(:slack, "UID")
+      assert identity.provider == "slack"
+      assert identity.uid == "UID"
+    end
+
+    test "returns a not found error tuple if not found" do
+      assert {:error, :not_found} = Identity.for_uid(:slack, "blah")
+    end
+  end
+
+  describe ".find_or_create_from_auth with Slack" do
+    @auth %{
+      credentials: %{token: "SLACK_TOKEN"},
+      other: %{user: "jimmyp", user_id: "USLACKUID"},
+      provider: :slack
+    }
+
+    test "creates a new identity" do
+      {:ok, identity} = Identity.find_or_create_from_auth(@auth)
+
+      assert identity.provider == "slack"
+      assert identity.access_token == "SLACK_TOKEN"
+      assert identity.uid == "USLACKUID"
+      assert identity.username == "jimmyp"
+    end
+
+    test "updates the identity for the provider and access_token" do
+      {:ok, identity} =
+        Identity.create(%{
+          provider: "slack",
+          uid: "BLAH",
+          access_token: "SLACK_TOKEN"
+        })
+
+      {:ok, found_identity} = Identity.find_or_create_from_auth(@auth)
+
+      assert identity.id == found_identity.id
+      assert found_identity.uid == "USLACKUID"
+    end
+
+    test "updates the identity for the provider and uid" do
+      {:ok, identity} =
+        Identity.create(%{
+          provider: "slack",
+          uid: "USLACKUID",
+          access_token: "BLAH"
+        })
+
+      {:ok, found_identity} = Identity.find_or_create_from_auth(@auth)
+
+      assert identity.id == found_identity.id
+      assert found_identity.access_token == "SLACK_TOKEN"
+    end
+  end
 end
